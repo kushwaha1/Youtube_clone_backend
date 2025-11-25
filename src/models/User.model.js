0c2c1da5e0562258c41902fb6d76330d5ca5027a
Sweety <rsweet375@gmail.com>
2025-11-25 10:29:57 +0530

    created user schema

---
+++ src/models/User.model.js
@@ -0,0 +1,12 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    username: { type: String, required: true, lowercase: true, unique: true },
+    email: { type: String, required: true, unique: true, lowercase: true, trim: true },
+    password: { type: String, required: true },
+    avatar: { type: String, default: 'https://example.com/avatar/default.png' },
+    createdAt: { type: Date, default: Date.now() }
+})
+
+const User = mongoose.model("User", userSchema);
+export default User;
