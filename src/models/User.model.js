4901c16803dcb978000ff36447f811c44568945e
Sweety <rsweet375@gmail.com>
2025-11-27 13:27:45 +0530

    add name in user model and change default url for avatar

--- src/models/User.model.js
+++ src/models/User.model.js
@@ -1,10 +1,11 @@
 import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema({
+    name: { type: String, required: true, trim: true },
     userName: { type: String, required: true, lowercase: true, unique: true },
     email: { type: String, required: true, unique: true, lowercase: true, trim: true },
     password: { type: String, required: true },
-    avatar: { type: String, default: 'https://example.com/avatar/default.png' },
+    avatar: { type: String, default: 'https://i.pinimg.com/1200x/cd/4b/d9/cd4bd9b0ea2807611ba3a67c331bff0b.jpg' },
     channels: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Channel' }],
     createdAt: { type: Date, default: Date.now() }
 })
