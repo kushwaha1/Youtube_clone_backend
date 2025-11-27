578a9528f4127f800ff5ce7617ada8f2ecc73351
Sweety <rsweet375@gmail.com>
2025-11-27 13:28:57 +0530

    update changes in auth routes for avatar upload and add routes in index file

--- index.js
+++ index.js
@@ -1,12 +1,23 @@
 import { configDotenv } from 'dotenv';
 import express from 'express';
 import { connectDB } from './src/config/db.js';
+import authRoutes from "./src/routes/auth.routes.js";
+import morgan from 'morgan';
+import cors from 'cors';
 
 configDotenv();
 connectDB();
 
 const app = express();
+
+app.use(cors());
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(morgan("dev"));
+
+
+// Route middlewares
+app.use('/api/auth', authRoutes);      // Auth routes: /auth/*
 
 const PORT = process.env.PORT || 5000; // Server port
 app.listen(PORT, () => {
--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,10 +1,11 @@
 import express from "express";
-import { login, register } from "../controller/auth.controller.js";
+import { login, register } from "../controllers/auth.controller.js";
+import { upload } from "../middleware/multer.js";
 
 const router = express.Router();
 
 // Route for user registration
-router.post('/register', register);
+router.post('/register', upload.single("avatar"), register);
 
 // Route for user login
 router.post('/login', login);
